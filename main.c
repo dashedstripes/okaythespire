b2692344670f298fe5873dafcdd217b6ece9fb4e
Adam Gray <adamgrayscale@gmail.com>
2024-02-03 16:22:45 +0000

    add a box

--- main.c
+++ main.c
@@ -13,8 +13,6 @@ int main()
     return -1;
   }
 
-  printf("SDL initialized.\n");
-
   SDL_Window *window = SDL_CreateWindow("okaythespire",
     SDL_WINDOWPOS_UNDEFINED,
     SDL_WINDOWPOS_UNDEFINED,
@@ -29,11 +27,21 @@ int main()
     return -1;
   }
 
-  printf("Window created.\n");
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (!renderer)
+  {
+    printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return -1;
+  }
 
   // Event loop
   SDL_Event e;
   int quit = 0;
+
+  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
   while (!quit)
   {
     while (SDL_PollEvent(&e) != 0)
@@ -43,12 +51,23 @@ int main()
         quit = 1;
       }
     }
-    // Your rendering or update logic here
 
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
+    // Render a blue rectangle
+    SDL_Rect rect = {200, 150, 240, 180};               // x, y, width, height
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Blue
+    SDL_RenderFillRect(renderer, &rect);                // Use SDL_RenderDrawRect for an unfilled rect
+
+    // Update screen
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(16); // Roughly 60 frames per second
   }
 
   printf("Quitting...\n");
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
